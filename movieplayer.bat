19f6bd8eee3322ab528eb8d74d87e271e289751f
Justin Dial <justinalandial@gmail.com>
2016-06-27 14:35:29 -0500

    Update movieplayer.bat

--- movieplayer.bat
+++ movieplayer.bat
@@ -1,7 +1,5 @@
 @ECHO off
-mode 75,40
-color 1F
-Title Movie Drive
+mode 75,40 && color 1F && Title Movie Drive
 dir /b "%cd%/Movies/" > movies.txt
 :Menu
 ECHO .......!!!Movie Drive!!!.........
@@ -11,6 +9,5 @@ ECHO .....................................................................
 SET /P M=Type 1, 2, .. then press ENTER:
 for /F "skip=%M% delims=" %%i in (movies.txt) do (Start %cd%\VLCPortable\App\vlc\vlc.exe -vvv "%cd%\Movies\%%i" --fullscreen & GOTO Menu)
 :incrementor
-echo (%count%)%1
-set /a count+=1
+echo (%count%)%1 && set /a count+=1
 GOTO :EOF
